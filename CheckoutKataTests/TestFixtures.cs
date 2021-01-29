865ace5851e1f881dc17c9e84dce202e92759abd
NicholasThurgoland <65541042+NicholasThurgoland@users.noreply.github.com>
2021-01-29 09:13:01 +0000

    Refactored Test Fixtures Out

---
+++ CheckoutKataTests/TestFixtures.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKataTests
+{
+    public class TestFixtures
+    {
+        public Item itemA = new Item()
+        {
+            SKU = "A",
+            UnitPrice = 10
+        };
+        public Item itemB = new Item()
+        {
+            SKU = "B",
+            UnitPrice = 15
+        };
+        public Item itemD = new Item()
+        {
+            SKU = "D",
+            UnitPrice = 55
+        };
+    }
+}
--- CheckoutKataTests/Tests.cs
+++ CheckoutKataTests/Tests.cs
@@ -4,33 +4,14 @@ namespace CheckoutKataTests
 {
     public class Tests
     {
-        private Item itemA = new Item()
-        {
-            SKU = "A",
-            UnitPrice = 10
-        };
-        private Item itemB = new Item() 
-        {
-            SKU = "B",
-            UnitPrice = 15
-        };
-        private Item itemD = new Item() 
-        {
-            SKU = "D",
-            UnitPrice = 55
-        };
-
         private Basket basket = new Basket();
 
-        [SetUp]
-        public void Setup()
-        {
-        }
+        private TestFixtures testFixtures = new TestFixtures();
 
         [Test]
         public void GivenAnItem_AddToBasket_BasketHasItem()
         {
-            basket.ScanItem(itemA);
+            basket.ScanItem(testFixtures.itemA);
 
             Assert.IsNotNull(basket);
         }
@@ -38,16 +19,16 @@ namespace CheckoutKataTests
         [Test]
         public void GivenItems_AddItemsToBasket_GetTotalPriceForItems()
         {
-            basket.ScanItem(itemA);
-            basket.ScanItem(itemB);
+            basket.ScanItem(testFixtures.itemA);
+            basket.ScanItem(testFixtures.itemB);
 
-            Assert.AreEqual(basket.GetTotalPrice(), itemA.UnitPrice + itemB.UnitPrice);
+            Assert.AreEqual(basket.GetTotalPrice(), testFixtures.itemA.UnitPrice + testFixtures.itemB.UnitPrice);
         }
 
         [Test]
         public void GivenThreeItemB_AddToBasket_PromotionAdded()
         {
-            basket.ScanItem(itemB, 3);
+            basket.ScanItem(testFixtures.itemB, 3);
 
             Assert.AreEqual(basket.GetTotalPrice(), 40);
         }
@@ -55,7 +36,7 @@ namespace CheckoutKataTests
         [Test]
         public void GivenTwoItemD_AddToBasket_25PercentOff()
         {
-            basket.ScanItem(itemD, 2);
+            basket.ScanItem(testFixtures.itemD, 2);
 
             Assert.AreEqual(basket.GetTotalPrice(), 82);
         }
